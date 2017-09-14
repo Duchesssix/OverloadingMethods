6d995f98d0fc2c3d407f0a25c205f9f617d2f82c
YOUR_PUBLIC_NAME <YOUR_PUBLIC@GIT_EMAIL.COM>
2017-09-13 22:13:50 -0500

    Added Comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int option = 0;
-        String business = null, name = null, phone = null;
-        Scanner input = new Scanner(System.in);
+        int option = 0;   //declare option variable to get user selection
+        String business = null, name = null, phone = null;  //declare variables for user input
+        Scanner input = new Scanner(System.in);  //declare scanner for user input
 
         do {
+           //display selection menu
             System.out.println("1. Business Customer");
             System.out.println("2. Residential Customer");
             System.out.println("3. Exit");
 
+            //get user selection
             option = input.nextInt();
 
+            //determine which prompts to display based on user selection
             switch (option) {
-                case 1:
+                case 1:  //Business Customer
                     System.out.println("Please enter business name:");
                     business = input.next();
                     System.out.println("Please enter contact name:");
@@ -25,7 +28,7 @@ public class Main {
                     phone = input.next();
                     showInfo(business, name, phone);
                     break;
-                case 2:
+                case 2:  //Residential Customer
                     System.out.println("Please enter contact name:");
                     name = input.next();
                     System.out.println("Please enter phone number (###) ###-####:");
@@ -40,7 +43,7 @@ public class Main {
         } while (option < 1 || option > 3);
     }
 
-
+    //Methods to display user input based on menu selection
     public static void showInfo(String business, String name, String phone) {
         System.out.println("Business name: " + business);
         System.out.println("Contact name: " + name);
